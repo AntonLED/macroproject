255aed867b92270b79233615ad54d117c4833ee8
AntonLED <al3406099@gmail.com>
2023-05-21 02:58:58 +0300

    add Auction functional not tested enought

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
-from volttron.platform.transactions.poly_line import PolyLine
-from volttron.platform.transactions.point import Point
+from volttron.platform.transactions.auction import Auction, PolyLine, Point
 import matplotlib.pyplot as plt 
 
+auct = Auction("test")
 
 curve1 = PolyLine()
 curve2 = PolyLine()
+
+_curve = PolyLine()
+_curve.add(Point(0, 1))
+_curve.add(Point(0.5, 1))
+_curve.add(Point(0.5, 0.8))
+_curve.add(Point(1.0, 0.8))
+_curve.add(Point(1.0, 0.3))
+_curve.add(Point(1.5, 0.3))
+_curve.add(Point(2.0, 0.3))
+_curve.add(Point(2.0, 0.1))
+
 
 curve1.add(Point(0, 0))
 curve1.add(Point(1, 1))
@@ -12,9 +23,14 @@ curve2.add(Point(0, 0.5))
 curve2.add(Point(1, 0.5))
 curve = PolyLine.uniform([curve1, curve2])
 
+print('\n')
+print(auct.get_result(_curve, [curve1, curve2]))
+print('\n')
+
 # plt.plot(curve1.vectorize()[0], curve1.vectorize()[1])
 # plt.plot(curve2.vectorize()[0], curve2.vectorize()[1])
-# plt.plot(curve.vectorize()[0], curve.vectorize()[1])
-# plt.show()
+plt.grid()
+plt.plot(curve.vectorize()[0], curve.vectorize()[1])
+plt.plot(_curve.vectorize()[0], _curve.vectorize()[1])
+plt.show()
 
-print(PolyLine.intersection(curve1, curve))
--- transactions/auction.py
+++ transactions/auction.py
@@ -1,3 +1,14 @@
+from volttron.platform.transactions.poly_line import PolyLine
+from volttron.platform.transactions.point import Point
+
+
 class Auction():
     def __init__(self, name) -> None:
         self.name = name
+
+    def get_result(self, self_demand_curve: PolyLine, bidders_curves: list[Point]) -> Point:
+        try:
+            aggregated_bidders_curve = PolyLine.uniform(bidders_curves)
+            return PolyLine.intersection(self_demand_curve, aggregated_bidders_curve)
+        except BaseException:
+            return None
